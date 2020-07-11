61d69def22541222b94137bed6447b45076f5fd4
Attila Domokos <adomokos@gmail.com>
2020-07-11 11:38:44 -0500

    Add quicksort logic assertions

--- tests/algorithms/test_quicksort.py
+++ tests/algorithms/test_quicksort.py
@@ -1,5 +1,3 @@
-import pytest
-
 def sum(xs):
     if xs == []:
         return 0
@@ -7,12 +5,14 @@ def sum(xs):
         head, *tail = xs
         return head + sum(tail)
 
+
 def count(xs):
     if xs == []:
         return 0
     else:
         head, *tail = xs
         return 1 + count(tail)
+
 
 def quicksort(xs):
     if len(xs) < 2:
@@ -28,15 +28,15 @@ class TestClass(object):
     def test_sum(self):
         result = sum([])
         assert result == 0
-        result = sum([1,2,3])
+        result = sum([1, 2, 3])
         assert result == 6
 
     def test_count(self):
         result = count([])
         assert result == 0
-        result = count([1,2,3,4])
+        result = count([1, 2, 3, 4])
         assert result == 4
 
     def test_quicksort(self):
-        result = quicksort([3,4,1,2])
-        assert result == [1,2,3,4]
+        result = quicksort([3, 4, 1, 2])
+        assert result == [1, 2, 3, 4]
