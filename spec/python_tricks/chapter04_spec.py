66c05fc3fac15148e0b365a9452647b5b08c07ec
Attila Domokos <adomokos@gmail.com>
2020-09-18 22:19:03 -0500

    Instance, Static, Class methods

--- spec/python_tricks/chapter04_spec.py
+++ spec/python_tricks/chapter04_spec.py
@@ -1,6 +1,8 @@
 from mamba import description, it, fit, context  # type: ignore
 from expects import expect, equal, raise_error  # type: ignore
+from typing import List, Type
 import copy
+import math
 
 
 class BaseValidationError(ValueError):
@@ -15,11 +17,38 @@ class NameTooLongError(BaseValidationError):
     pass
 
 
-def validate(name):
+def validate(name: str):
     if len(name) < 5:
         raise NameTooShortError(name)
     if len(name) > 10:
         raise NameTooLongError(name)
+
+
+class Pizza:
+    def __init__(self: 'Pizza', radius: int, ingredients: List[str]):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self: 'Pizza') -> str:
+        return f'Pizza({self.ingredients!r})'
+
+    def get_ingredients(self: 'Pizza') -> List[str]:
+        return self.ingredients
+
+    def area(self: 'Pizza') -> float:
+        return self.circle_area(self.radius)
+
+    @classmethod
+    def margherita(cls: Type['Pizza']) -> 'Pizza':
+        return cls(4, ['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls: Type['Pizza']) -> 'Pizza':
+        return cls(5, ['mozzarella', 'tomatoes', 'ham'])
+
+    @staticmethod
+    def circle_area(r: int) -> float:
+        return r ** 2 * math.pi
 
 
 with description('Chapter04') as self:
@@ -44,7 +73,7 @@ with description('Chapter04') as self:
             xs[1][0] = 9
             expect(xs[1][0]).to(equal(ys[1][0]))
 
-        with fit('deep copy'):
+        with it('deep copy'):
             xs = [[1, 2, 3], [4, 5, 6]]
             zs = copy.deepcopy(xs)
             expect(len(xs)).to(equal(len(zs)))
@@ -53,3 +82,9 @@ with description('Chapter04') as self:
             xs[1][0] = 9
             assert xs[1][0] == 9
             assert zs[1][0] == 4
+
+    with context('class and static methods'):
+        with fit('can have different roles'):
+            p = Pizza.margherita()
+            expect(p.ingredients).to(equal(['mozzarella', 'tomatoes']))
+            assert p.area() > 0
