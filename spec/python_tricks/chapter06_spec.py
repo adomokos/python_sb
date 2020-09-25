0169c1941f39bd2613835519f370413c3571bbee
Attila Domokos <adomokos@gmail.com>
2020-09-25 09:15:43 -0500

    Slice like a king

--- spec/python_tricks/chapter06_spec.py
+++ spec/python_tricks/chapter06_spec.py
@@ -15,3 +15,25 @@ with description('Chapter06') as self:
         with it('works with dict'):
             d = {x: x * x for x in range(1, 4)}
             assert d == {1: 1, 2: 4, 3: 9}
+
+    with context('slicing'):
+        with it('works like this: lst[start:end:step]'):
+            lst = range(1, 6)
+            assert list(lst[1:3:1]) == [2, 3]
+
+        with it('step defaults to one'):
+            lst = range(1, 6)
+            assert list(lst[1:3]) == [2, 3]
+
+        with it('can create a list with steps'):
+            lst = range(1, 6)
+            assert list(lst[::2]) == [1, 3, 5]
+
+        with it('can reverse a list'):
+            lst = range(1, 6)
+            assert list(lst[::-1]) == [5, 4, 3, 2, 1]
+
+        with it('can clear a list'):
+            lst2 = [1, 2, 3, 4, 5]
+            del lst2[:]
+            assert lst2 == []
