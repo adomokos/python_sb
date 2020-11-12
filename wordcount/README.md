199184a8cf578cbff7934e84c639e5c6d981b4e2
Attila Domokos <adomokos@gmail.com>
2020-11-11 20:52:23 -0600

    Remove wordcount

--- wordcount/README.md
+++
@@ -1,3 +0,0 @@
-# Wordcount
-
-Source from [here](https://realpython.com/flask-by-example-part-1-project-setup/).
--- wordcount/tests/test_flask_by_example.py
+++
@@ -1,5 +0,0 @@
-from flask_by_example import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
